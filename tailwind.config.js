055b37fb04afcfcf244aaab24ee91222489f4a7f
Jordan Hanuise <jordanhanuise@hotmail.com>
2024-11-16 23:45:35 +0100

    changement samedi soir

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
       colors: {
         primary: '#E31C3D',   // Rouge vif
         dark: '#0D0203',      // Noir profond
-        light: 'rgb(255, 250, 250)',     // Blanc cassé
+        light: '#FFFAFA',     // Blanc cassé
         teal: '#003844',      // Bleu/vert foncé
         slate: '#465775',     // Bleu ardoise
       
